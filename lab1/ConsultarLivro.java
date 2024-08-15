cb28b498a168b994812f7ec506696febdae29daf
Anderson Nicodemo <125095374+Andernicod@users.noreply.github.com>
2024-08-15 23:32:56 +0000

    lab 1

---
+++ lab1/ConsultarLivro.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ConsultarLivro{
+    public static void main(String[] args) {
+        Path arq_livros = Paths.get("/workspaces/progsistemasII/lab1/livros.txt");
+        try{
+            String livros[] = Files.readAllLines(arq_livros).toArray(new String[0]);
+            for (String  livro: livros){
+                System.out.println(livro);
+            }
+        } catch(IOException ioex1){
+            ioex1.printStackTrace();
+        }
+    }
+}
